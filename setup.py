61362ee6b40beb33a4a2f2266bd84ed0feb4d731
Christopher Brown <pyrrhicpachyderm@gmail.com>
2020-01-19 15:04:26 +1300

    Add a setup script: setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+	name = "pluribus",
+	version = "0.1.0",
+	packages=["pluribus"],
+	entry_points = {
+		"console_scripts": [
+			"pluribus = pluribus.__main__:main"
+		]
+	},
+)
