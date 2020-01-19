d35175fe52a55ab415d4a6a79ccdedbf829ce209
Christopher Brown <pyrrhicpachyderm@gmail.com>
2020-01-19 14:58:48 +1300

    Add pluribus/input.py, and parse Pluribus' argument.

--- pluribus/__main__.py
+++ pluribus/__main__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import sys
+from pluribus.input import get_config_file_path
 
 def main(args=None):
 	if args is None:
 		args = sys.argv
+	
+	config_file_path = get_config_file_path(args)
 
 if __name__ == "__main__":
 	main()
---
+++ pluribus/input.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+def print_usage_error(args):
+	print("Usage: {} <config file>".format(args[0]), file=sys.stderr)
+	exit(1)
+
+def get_config_file_path(args):
+	if(len(args) != 2):
+		print_usage_error(args)
+	else:
+		return args[1]
