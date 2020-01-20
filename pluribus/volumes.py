7562b2ae55a1fbd3143c991effa50e2137d87942
Christopher Brown <pyrrhicpachyderm@gmail.com>
2020-01-20 16:20:47 +1300

    Define the Volume class.

---
+++ pluribus/volumes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from pluribus.options import set_options
+
+class Volume:
+	_options_default = {
+		"title":None,
+		"content":None,
+	}
+	
+	_options_type = {
+		"title":str,
+		"content":str,
+	}
+	
+	def __init__(self, volume_dict, all_dict):
+		set_options(self, self._options_type, [volume_dict, all_dict, self._options_default])
