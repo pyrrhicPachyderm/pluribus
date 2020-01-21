5919396399abb6a99faf6b3fdaac1f8bc980d845
Christopher Brown <pyrrhicpachyderm@gmail.com>
2020-01-21 14:13:58 +1300

    Parse the config file into a Config and VolumeContainer instance.

--- pluribus/__main__.py
+++ pluribus/__main__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 from pluribus.input import get_config_file_path, read_config_file
+from pluribus.volumes import VolumeContainer
+from pluribus.config import Config
 
 def main(args=None):
 	if args is None:
@@ -8,6 +10,8 @@ def main(args=None):
 	
 	config_file_path = get_config_file_path(args)
 	config_dict = read_config_file(config_file_path)
+	volume_container = VolumeContainer(config_dict)
+	config = Config(config_dict)
 
 if __name__ == "__main__":
 	main()
