479cc232c8dbec9f48e50e768a61f61d16eb7645
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-01-28 16:47:38 +1300

    Add comments to clarify the operation of VolumeContainer.__init__().

--- pluribus/volumes.py
+++ pluribus/volumes.py
@@ -22,7 +22,10 @@ class VolumeContainer:
 	volumes = dict()
 	
 	def __init__(self, config_dict):
+		#First, get the options that should be applied to all volumes.
 		all_dict = config_dict.get("all", dict())
+		#Next, get the individual (non-reserved) volumes.
+		#Volumes are represented by nested dictionaries within the config dictionary.
 		for key, value in config_dict.items():
 			if isinstance(value, dict) and not key in self._reserved_tags:
 				self.volumes[key] = Volume(key, value, all_dict)
